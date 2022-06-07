eb0f70b940cd306cf9ff9c98730630753dc9a8b6
Inamul <inamulhoque097@gmail.com>
2022-06-07 23:31:00 +0530

    Initial Commit

---
+++ src/com/inamul/Gambler.java
@@ -0,0 +1,13 @@
+package com.inamul;
+
+public class Gambler {
+
+    public static void DailyGamble(){
+        int dailyStake = 100;
+        int perBet = 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambler's club.");
+    }
+}
