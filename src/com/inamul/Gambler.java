cfbb789246128c04411c611bf9e20b4396b3c906
Inamul <inamulhoque097@gmail.com>
2022-06-08 00:13:34 +0530

    Added: Make  bet so either win or loss

--- src/com/inamul/Gambler.java
+++ src/com/inamul/Gambler.java
@@ -1,13 +1,28 @@
 package com.inamul;
 
 public class Gambler {
+    static int dailyStake = 100;
+    static int perBet = 1;
 
     public static void DailyGamble(){
-        int dailyStake = 100;
-        int perBet = 1;
+        int initialBet = 1;
+        int check = (int) (Math.floor(Math.random()*10)%2+1);
+        if (check == 1) {
+            initialBet += 1;
+            System.out.println("Win");
+            System.out.println("Amount: " + initialBet);
+        } else {
+                initialBet-=1;
+                if (initialBet == 0){
+                    System.out.println("No money to play.");
+                } else {
+                    System.out.println("Amount: "+initialBet);
+                }
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambler's club.");
+        DailyGamble();
     }
 }
